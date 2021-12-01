c5d022d57085fd3c13de6c5ce9309df46ab34097
varun07neu <90414600+varun07neu@users.noreply.github.com>
2021-12-01 12:26:21 -0500

    Msd radix sort for pinyin (#1)
    * Initial Commit:
    1) Implemented MSD Radix sort for Unicode Characters
    2) Added Sort Utils for general sort utility functions and variables
    3) Added Dependencies for Pinyin4j
    4) Converted original repo into Java Project
    5) Added Driver class to execute code
    
    * Cleared unnecessary exceptions
    
    * Moved modules to appropriate positions

---
+++ src/main/edu/neu/info6205/utils/SortUtils.java
@@ -0,0 +1,41 @@
+package edu.neu.info6205.utils;
+
+import net.sourceforge.pinyin4j.PinyinHelper;
+import net.sourceforge.pinyin4j.format.HanyuPinyinCaseType;
+import net.sourceforge.pinyin4j.format.HanyuPinyinOutputFormat;
+import net.sourceforge.pinyin4j.format.HanyuPinyinToneType;
+import net.sourceforge.pinyin4j.format.HanyuPinyinVCharType;
+import net.sourceforge.pinyin4j.format.exception.BadHanyuPinyinOutputFormatCombination;
+
+public class SortUtils {
+    public static String[] convertToPinyin(String[] a) throws BadHanyuPinyinOutputFormatCombination {
+        String[] temp = new String[a.length];
+        HanyuPinyinOutputFormat format = new HanyuPinyinOutputFormat();
+        format.setToneType(HanyuPinyinToneType.WITHOUT_TONE);
+        format.setCaseType(HanyuPinyinCaseType.LOWERCASE);
+        format.setVCharType(HanyuPinyinVCharType.WITH_U_UNICODE);
+        long length = a.length;
+        for(int i =0; i< length; i++){
+            String s = a[i];
+            String tempString ="";
+            for(int j=0; j<s.length();j++){
+                String[] pinArr = PinyinHelper.toHanyuPinyinStringArray(s.charAt(j), format);
+                tempString = tempString.concat(pinArr[0]);
+            }
+            temp[i] = tempString;
+        }
+        return temp;
+    }
+
+    public static String[] chineseExample ={
+                "刘持平",
+                "洪文胜",
+                "樊辉辉",
+                "苏会敏",
+                "高民政",
+                "曹玉德",
+                "袁继鹏",
+                "阿鼎",
+                "阿"
+    };
+}
