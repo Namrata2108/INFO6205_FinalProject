d6f9a2b662bee6c2aa4043029483c87a3e1560ad
Varun <gowda.v@northeastern.edu>
2021-12-01 12:44:36 -0500

    Added Dual-Pivot quick sort for pinyin Updated Sort Utils

---
+++ src/main/edu/neu/info6205/sorts/DualPivotQuickSort.java
@@ -0,0 +1,65 @@
+package edu.neu.info6205.sorts;
+
+import edu.neu.info6205.utils.SortUtils;
+
+import java.text.CollationKey;
+import java.text.Collator;
+import java.util.Locale;
+
+public class DualPivotQuickSort {
+
+
+
+    public static void sort (String[] arr, int low, int high){
+        if(low < high){
+            int[] pivot = partition(arr, low, high);
+
+            sort(arr, low, pivot[0] -1);
+            sort(arr, pivot[0]+1, pivot[1]-1);
+            sort(arr, pivot[1]+1, high);
+
+        }
+    }
+
+    private static int[] partition(String[] arr, int low, int high){
+
+        if(SortUtils.greaterThan(arr[low], arr[high])){
+            SortUtils.swap(arr, low, high);
+        }
+
+        int j = low+1;
+        int g = high - 1;
+        int k = low + 1;
+        String p = arr[low];
+        String q = arr[high];
+
+        while (k <= g){
+            if(SortUtils.lessThan(arr[k], p)){
+                SortUtils.swap(arr, k, j);
+                j++;
+            }
+
+            else if (SortUtils.greaterThanOrEqualTo(arr[k], q)){
+                while(SortUtils.greaterThan(arr[g], q) && (k < g)){
+                    g--;
+                }
+                SortUtils.swap(arr, k,g);
+                g--;
+                if(SortUtils.lessThan(arr[k], p)){
+                    SortUtils.swap(arr, k, j);
+                }
+            }
+            k++;
+
+        }
+        j--;
+        g++;
+
+        SortUtils.swap(arr, low, j);
+        SortUtils.swap(arr, high, g);
+
+        return new int[] {j,g};
+    }
+
+
+}
--- src/main/edu/neu/info6205/utils/SortUtils.java
+++ src/main/edu/neu/info6205/utils/SortUtils.java
@@ -7,7 +7,14 @@ import net.sourceforge.pinyin4j.format.HanyuPinyinToneType;
 import net.sourceforge.pinyin4j.format.HanyuPinyinVCharType;
 import net.sourceforge.pinyin4j.format.exception.BadHanyuPinyinOutputFormatCombination;
 
+import java.text.CollationKey;
+import java.text.Collator;
+import java.util.Locale;
+
 public class SortUtils {
+
+    private static Collator coll = Collator.getInstance(Locale.CHINA);
+
     public static String[] convertToPinyin(String[] a) throws BadHanyuPinyinOutputFormatCombination {
         String[] temp = new String[a.length];
         HanyuPinyinOutputFormat format = new HanyuPinyinOutputFormat();
@@ -38,4 +45,30 @@ public class SortUtils {
                 "阿鼎",
                 "阿"
     };
+    public static void swap(String[] arr, int i, int j){
+        String temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static boolean lessThan(String s1, String s2){
+        CollationKey ck1 = coll.getCollationKey(s1);
+        CollationKey ck2 = coll.getCollationKey(s2);
+        return (ck1.compareTo(ck2)<0);
+    }
+    public static boolean greaterThan(String s1, String s2){
+        CollationKey ck1 = coll.getCollationKey(s1);
+        CollationKey ck2 = coll.getCollationKey(s2);
+        return (ck1.compareTo(ck2)>0);
+    }
+    public static boolean greaterThanOrEqualTo(String s1, String s2){
+        CollationKey ck1 = coll.getCollationKey(s1);
+        CollationKey ck2 = coll.getCollationKey(s2);
+        return (ck1.compareTo(ck2)>=0);
+    }
+    public static boolean lessThanOrEqualTo(String s1, String s2){
+        CollationKey ck1 = coll.getCollationKey(s1);
+        CollationKey ck2 = coll.getCollationKey(s2);
+        return (ck1.compareTo(ck2)<=0);
+    }
 }
