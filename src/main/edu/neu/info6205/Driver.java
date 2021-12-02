db0ef6e56769121e40c1ec5e048d2e4159e5d64c
Namrata2108 <42319987+Namrata2108@users.noreply.github.com>
2021-12-02 01:16:43 -0500

    LSDRadixSort

--- src/main/edu/neu/info6205/Driver.java
+++ src/main/edu/neu/info6205/Driver.java
@@ -1,5 +1,6 @@
 package edu.neu.info6205;
 
+import edu.neu.info6205.sorts.LSDRadixSort;
 import edu.neu.info6205.sorts.MSDRadixSort;
 import edu.neu.info6205.utils.SortUtils;
 import net.sourceforge.pinyin4j.format.exception.BadHanyuPinyinOutputFormatCombination;
@@ -11,10 +12,7 @@ public class Driver {
          */
         String[] chinese =SortUtils.chineseExample;
         String[] chineseConv = SortUtils.convertToPinyin(chinese);
-        MSDRadixSort.sort(chineseConv, chinese);
+        LSDRadixSort.sort(chineseConv, chinese);
         int n = chinese.length;
-        for (int i = 0; i < n; i++) {
-            System.out.println(chinese [i]);
-        }
     }
 }
