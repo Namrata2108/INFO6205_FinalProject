49eb9ee1137f8f028ee5227be4bfee1e6fb609e4
Namrata2108 <42319987+Namrata2108@users.noreply.github.com>
2021-12-03 22:16:23 -0500

    Tim Sort test cases (#6)

---
+++ src/test/edu/neu/info6205/sorts/TimSortTest.java
@@ -0,0 +1,31 @@
+package edu.neu.info6205.sorts;
+
+
+import edu.neu.info6205.utils.SortUtils;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
+public class TimSortTest {
+    @Test
+    public void testSort() {
+        String[] chinese = Arrays.copyOf(SortUtils.chineseExample, SortUtils.chineseExample.length);
+        TimSort.doSort(chinese);
+        for (int i = 0; i < chinese.length; i++) {
+            assertEquals(chinese[i], SortUtils.chineseExampleSorted[i]);
+        }
+    }
+
+    @Test
+    public void readFileSortTest() {
+        String[] chinese = SortUtils.readFromFile("src/resources/chineseExample.txt");
+        assertEquals(chinese.length,SortUtils.chineseExample.length);
+        TimSort.doSort(chinese);
+        for (int i = 0; i < chinese.length; i++) {
+            assertEquals(chinese[i], SortUtils.chineseExampleSorted[i]);
+        }
+    }
+}
+
